f883783e20404a6f280282c19cac1b676fbac489
warren10hk <warren10hk@yahoo.com.hk>
2018-05-09 17:18:35 +0800

    Add formatted data

---
+++ crawler-python/gsmarena/format-data.py
@@ -0,0 +1,91 @@
+import json
+import re
+
+new_data = []
+with open("unformatted-data.json", "r") as input_file:
+    data = json.loads(input_file.read())
+
+for i in data:
+    temp = {
+        "brand": i["brand"],
+        "model": i["model"],
+        "announced": {
+            "year": "",
+            "month": ""
+        },
+        "apperance": {
+            "depth": i["Dimension (Depth)"],
+            "height": i["Dimension (Height)"],
+            "width": i["Dimension (Width)"],
+            "weight": i["Weight"],
+            "colors": i["Colors"]
+        },
+        "display": {
+            "screen": i["Screen"],
+            "size": i["Screen Size"],
+            "ratio": i["Ratio"],
+            "screen_to_body_ratio": i["Screen To Body Ratio"],
+            "PPI": i["PPI"],
+            "reslolution": [i["Resolution (Height)"], i["Resolution (width)"]]
+        },
+        "specification": {
+            "CPU": i["Chipset"],
+            "CPU_speed": i["CPU Speed"],
+            "GPU": i["GPU"],
+            "RAM": i["RAM"],
+            "ROM": i["ROM"],
+            "camera": {
+                "front": {
+                    "resolution": [],
+                    "aperture": i["Front Camera f"]
+                },
+                "rear":  {
+                    "resolution": [],
+                    "aperture": i["Rear Camera 1 f"]
+                }
+            },
+            "battery": {
+                "capacity": i["Battery Capacity"],
+                "removable": i["Battery Removable"]
+            },
+            "radio": i["Radio"],
+            "USB": i["USB"],
+            "card_slot": {
+                "micro_sd": i["Card Slot"],
+                "max": i["microSD Max"],
+                "dedicated": i["Dedicated SD Card Slot"]
+            }
+        },
+        "connectivity": {
+            "2G_bands": {
+                "GSM": re.findall("\d+", i["2G bands"])
+            },
+            "3G_bands": {
+                "HSDPA": re.findall("\d+", i["3G bands"])
+            },
+            "4G_bands": {
+                "FD-LTE": re.findall("\((\d+)\)", i["4G bands"])
+            },
+            "SIM1": i["SIM 1"],
+            "SIM2": i["SIM 2"],
+            "dial_SIM": i["Dual Sim"],
+            "hybrid_SIM": i["Hybrid SIM"],
+            "WLAN": i["WLAN"],
+            "GPS": "",
+            "bluetooth": i["Bluetooth"],
+            "NFC": i["NFC"],
+            "3.5mm jack": i["3.5mm jack"],
+            "Infrared port": i["Infrared port"]
+        },
+        "system": {
+            "OS": i["OS"],
+            "version": i["Version"]
+        },
+        "fingerprint": i["Fingerprint"]
+    }
+
+    new_data.append(temp)
+
+with open("formatted_data.json", "w") as output_file:
+    json.dump(new_data, output_file)
+    
