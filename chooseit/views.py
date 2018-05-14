73f938ab6f6b68c6211d49d4cf53f83f0c5fd253
warren10hk <warren10hk@live.hk>
2018-05-15 03:00:55 +0800

    Add top rated phones on homepage

--- chooseit/views.py
+++ chooseit/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render, render_to_response
 from django.db.models import Q
 from phone.models import Phone
+from rating.models import Rating
+
 # Create your views here.
 
 def index(req):
@@ -21,5 +23,19 @@ def index(req):
         }
         return render(req, 'listofphones.html', ctx)
 
+    rating_table = {}
+    rating_data = Rating.objects.all()
+    for data in rating_data.values_list():
+        uid, pid, rate = data[1]-1, data[2]-1, data[3]
+        if rating_table.get(pid, "empty"):
+            rating_table[pid] = []
+        rating_table[pid].append(rate)
+    
+    avg_score = {}
+    for i in rating_table:
+        avg_score[i] = (sum(rating_table[i])/len(rating_table[i]))
 
-    return render(req, 'index.html', {'accstatus' : req.user.is_authenticated, 'name' : req.user.username})
+    top_score = [i[0] for i in sorted(avg_score.items(), key=lambda x: -x[1])[:5]]
+    phoneobj = Phone.objects.filter(pid__in = top_score)
+
+    return render(req, 'index.html', {'accstatus' : req.user.is_authenticated, 'name' : req.user.username, "phoneinfo" : phoneobj})
