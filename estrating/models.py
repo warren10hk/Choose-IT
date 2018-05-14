5bd3b6cdd342b78a5a4d51fcbb07e01ff7f5f1ed
warren10hk <warren10hk@live.hk>
2018-05-15 00:43:00 +0800

    Add Estimate Rating table

---
+++ estrating/models.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+from appuser.models import Appuser
+from phone.models import Phone
+
+# Create your models here.
+
+class ESTRating(models.Model):
+    uid = models.ForeignKey(
+        Appuser,
+        to_field = "uid",
+        on_delete = models.PROTECT
+    )
+    pid = models.ForeignKey(
+        Phone,
+        to_field = "pid",
+        on_delete = models.PROTECT
+    )
+    rate = models.IntegerField(
+        default = 1
+    )
